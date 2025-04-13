3ac1b78444c6b2f5082d1b44407308b0d1e2b203
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2025-04-13 09:35:09 -0300

    🐛 Added auto generated id

--- src/main/java/com/smarterfit/model/Plan.java
+++ src/main/java/com/smarterfit/model/Plan.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -28,6 +30,7 @@ import lombok.Setter;
 @Builder
 public class Plan {
    @Id
+   @GeneratedValue(strategy = GenerationType.UUID)
    UUID id;
 
    @Column(name = "name", nullable = false)
