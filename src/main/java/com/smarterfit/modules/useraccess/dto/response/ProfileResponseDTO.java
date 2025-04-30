39f9dd80f018427e3b4e478a70bb03e8076d3d33
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2025-04-30 06:05:35 -0300

    🐛 Fixed bug

--- src/main/java/com/smarterfit/modules/useraccess/dto/response/ProfileResponseDTO.java
+++ src/main/java/com/smarterfit/modules/useraccess/dto/response/ProfileResponseDTO.java
@@ -7,7 +7,20 @@ import com.smarterfit.common.enums.Gender;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ProfileResponseDTO {
+    private UUID id;
+    private String fullName;
+    private String cpf;
+    private String phone;
     private LocalDate birthDate;
     private Gender gender;
     private AddressResponseDTO address;
